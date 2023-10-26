4f250d2f2047c5b202159d8ec5b72d6e0aecc47f
Jakub Motyka <jakub.motyka@daftmobile.com>
2023-10-27 01:27:11 +0200

    wi

--- wimu10/metrics.py
+++ wimu10/metrics.py
@@ -1,3 +1,11 @@
+import muspy as mp
+
 def dummy_metric():
     """Dummy metric."""
     pass
+
+
+def midi_markov(music: mp.Music):
+    for track in music.tracks:
+        for note in track.notes:
+            note.print()
