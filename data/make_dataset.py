0ca3457f883b8c367df66680d00ca582fd4c78de
MortonPL <bartm12@wp.pl>
2023-11-26 21:50:57 +0100

    Lint

--- data/make_dataset.py
+++ data/make_dataset.py
@@ -4,14 +4,15 @@ from argparse import ArgumentParser
 from os import makedirs
 from pathlib import Path
 
-DATA_RAW_PATH = "./data/raw/"
+DATA_RAW_PATH = './data/raw/'
 DATASETS = {
-    "lakh": mp.LakhMIDIDataset,
-    "maestro": mp.MAESTRODatasetV3,
-    "nesmdb": mp.NESMusicDatabase,
-    "musicnet": mp.MusicNetDataset,
-    "emopia": mp.EMOPIADataset,
+    'lakh': mp.LakhMIDIDataset,
+    'maestro': mp.MAESTRODatasetV3,
+    'nesmdb': mp.NESMusicDatabase,
+    'musicnet': mp.MusicNetDataset,
+    'emopia': mp.EMOPIADataset,
 }
+
 
 def download_muspy_midi(dataset_name: str):
     """Download a MusPy MIDI dataset specified by name.
@@ -28,8 +29,9 @@ def download_muspy_midi(dataset_name: str):
     makedirs(path, exist_ok=True)
     dataset(path, download_and_extract=True, cleanup=True)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("dataset", choices=DATASETS.keys(), help="MusPy MIDI dataset to download")
+    parser.add_argument('dataset', choices=DATASETS.keys(), help='MusPy MIDI dataset to download')
     args = parser.parse_args()
     download_muspy_midi(args.dataset)
