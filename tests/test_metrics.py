9be5077242bc63e675b8aaf8274eb71623a1d44c
Jakub Motyka <jakub.motyka@daftmobile.com>
2023-11-26 15:03:56 +0100

    wip

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,10 @@
-from wimu10 import dummy_metric
+from wimu10 import dummy_metric, midi_markov
 
 
 def test_dummy_metric():
     dummy_metric()
+
+
+
+def test_markov_metric():
+    midi_markov()
--- wimu10/metrics.py
+++ wimu10/metrics.py
@@ -5,7 +5,13 @@ def dummy_metric():
     pass
 
 
-def midi_markov(music: mp.Music):
-    for track in music.tracks:
+def midi_markov(track: mp.Track):
+    notess = []
+    print(track.notes)
+    for t in range(0, 160000, 100):
+        notes = []
         for note in track.notes:
-            note.print()
+            if note.start <= t and t <= note.end:
+                notes.append(note)
+        notess.append(notes)
+    print(notess)
