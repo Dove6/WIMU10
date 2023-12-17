8681b8d1dd87a12215d7bc50396bea2f27014dc8
Jan Walczak <j.walczak@interia.pl>
2023-12-17 14:28:22 +0100

    calculation and visualization of histogram and transition matrix

--- notebooks/harmonics.py
+++ notebooks/harmonics.py
@@ -6,14 +6,39 @@ from pathlib import Path
 import pypianoroll as piano
 import matplotlib.pyplot as plt
 import muspy as mp
+import seaborn as sns
+import numpy as np
 
 from setup_dataset import DATA_RAW_PATH, download_muspy_midi
 from wimu10 import chords_histogram, chords_transition_matrix, get_chords_list
 
-def visualize(music: mp.Music) -> None:
+def visualize_pianoroll(music: mp.Music) -> None:
     mp.show_pianoroll(music)
     plt.show()
 
+def visualize_chord_transition(chords_dict:dict, transition_matrix:np.ndarray) -> None:
+    labels = chords_dict.keys()
+    _, ax = plt.subplots()
+    im = ax.imshow(transition_matrix)
+    
+    # Create colorbar
+    ax.figure.colorbar(im, ax=ax)
+    
+    # Set labels
+    ax.set_xticks(np.arange(len(labels)), labels=labels)
+    ax.set_yticks(np.arange(len(labels)), labels=labels) 
+    plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
+             rotation_mode="anchor")
+    ax.invert_yaxis()
+    plt.title("Chord transition heatmap")
+    plt.show()
+
+def visualize_chord_histogram(chords_hist:dict):
+    plt.bar(chords_hist.keys(), chords_hist.values() )
+    plt.xticks(rotation=90)
+    plt.title("Chord histogram")
+    plt.show()
+    
 # Prepare music instance
 dataset = 'maestro'
 download_muspy_midi(dataset)
@@ -21,7 +46,14 @@ path = Path(DATA_RAW_PATH + dataset + '/_converted/0001.json')
 music = mp.load_json(path)
 track = music.tracks[0]
 
+
+### Transition matrix testing
+chords_dict, transition_matrix = chords_transition_matrix(track)
+visualize_chord_transition(chords_dict, transition_matrix)
+
+### Histogram testing
 chords = chords_histogram(track)
-for chord in chords.items():
-    print(chord )
-visualize(music)
+visualize_chord_histogram(chords)
+
+### Visualizing pianoroll
+visualize_pianoroll(music)
