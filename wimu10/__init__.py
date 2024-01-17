d6f9e164cf479c007b01c7c97524a28fd3781375
MiniaczQ <xnetroidpl@gmail.com>
2024-01-17 05:02:28 +0100

    fix ruff 3

--- wimu10/__init__.py
+++ wimu10/__init__.py
@@ -2,6 +2,7 @@ from . import metrics
 from . import metrics_chords
 from . import metrics_dynamics
 from .metrics import *
+from .metrics_chords import *
 from .metrics_dynamics import *
 
 __all__ = [  # noqa: F405
